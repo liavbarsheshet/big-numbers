49c3033b59ef6aa1acc66e4eada4ebd4d56564d1
Liav Barsheshet, LBDevelopments <liavbarsheshet@gmail.com>
2023-01-03 12:16:30 +0200

    Starting naive multipication.

--- big-number.hpp
+++ big-number.hpp
@@ -7,31 +7,43 @@
 #define BIG_NUMBERS_BIG_NUMBER_HPP
 namespace BigNumber {
     const static char *symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const static char *inf = "∞";
+    const static char *inf = "Infinity";
 
     const static int MAX_BASE = 37;
 
     typedef unsigned char digit;
 
-    class Error;
+    class BigNumberError : std::exception {
+    };
+
+    class base_difference : public BigNumberError {
+    };
+
+    class invalid_base : public BigNumberError {
+    };
+
+    class invalid_number : public BigNumberError {
+    };
+
+    class uncertainty : public BigNumberError {
+    };
 
     unsigned int ToIndex(const BigNumber::digit &c_digit);
 
     class UInt;
+
+    UInt operator+(const UInt &a, const UInt &b);
+
+    UInt operator-(const UInt &a, const UInt &b);
+
+    bool operator>=(const UInt &a, const UInt &b);
+
+    bool operator!=(const UInt &a, const UInt &b);
+
+    bool operator<=(const UInt &a, const UInt &b);
+
+    bool operator>(const UInt &a, const UInt &b);
 }
-
-
-class BigNumber::Error : public std::exception {
-    std::string message;
-public:
-    Error() : std::exception() {};
-
-    Error(const std::string &message) : message(message) {}
-
-    virtual const char *what() const noexcept override {
-        return this->message.c_str();
-    }
-};
 
 unsigned int BigNumber::ToIndex(const BigNumber::digit &c_digit) {
     unsigned int digit = c_digit;
@@ -43,7 +55,7 @@ unsigned int BigNumber::ToIndex(const BigNumber::digit &c_digit) {
     } else if (digit > 96 && digit < 123) {
         digit -= 87;
     } else {
-        throw BigNumber::Error("Invalid number.");
+        throw BigNumber::invalid_number();
     }
 
     return digit;
@@ -52,47 +64,106 @@ unsigned int BigNumber::ToIndex(const BigNumber::digit &c_digit) {
 
 class BigNumber::UInt {
 protected:
+    typedef enum {
+        SUB, ADD, MUL
+    } OPERATOR;
     std::vector<BigNumber::digit> number;
     const unsigned int base;
 
+private:
+    void RemoveOpeningZeros() {
+        auto it = this->number.rbegin();
+        while (this->number.size() > 1 && *it == BigNumber::symbols[0]) {
+            this->number.pop_back();
+            it = this->number.rbegin();
+        }
+    }
+
+    void BasicOP(const UInt &number, OPERATOR op) {
+        unsigned int carry = 0;
+
+        auto a_it = number.number.begin();
+        auto a_end = number.number.end();
+        auto b_it = this->number.begin();
+        auto b_end = this->number.end();
+
+        while (b_it != b_end || carry) {
+            unsigned int a_index = (a_it == number.number.end() ? 0 : BigNumber::ToIndex(*a_it));
+            unsigned int b_index = (b_it == this->number.end() ? 0 : BigNumber::ToIndex(*b_it));
+            BigNumber::digit digit;
+            unsigned int sum;
+            int sub;
+
+            switch (op) {
+                case ADD:
+                    sum = a_index + b_index + carry;
+                    digit = BigNumber::symbols[(sum) % (this->base)];
+                    carry = (sum) / this->base;
+                    break;
+                case SUB:
+                    sub = a_index - b_index - carry;
+                    carry = sub >= 0 ? 0 : carry;
+                    digit = sub >= 0 ? BigNumber::symbols[(sub) % (this->base)] : BigNumber::symbols[
+                            (sub + (this->base - (carry++))) % (this->base)];
+
+                    break;
+            }
+
+            if (a_it != a_end) {
+                ++a_it;
+            }
+            if (b_it != this->number.end()) {
+                *b_it = digit;
+                ++b_it;
+            } else {
+                this->number.push_back(digit);
+            }
+
+        }
+
+        this->RemoveOpeningZeros();
+    }
+
 public:
     explicit UInt() : number(), base(10) {
-        number.push_back('0');
+        number.push_back(BigNumber::symbols[0]);
     };
 
-    UInt(const UInt &a) : number(), base(a.base) {
-        for (auto a: a.number) {
-            this->number.push_back(a);
+    UInt(const UInt &number) : number(), base(number.base) {
+        for (auto digit: number.number) {
+            this->number.push_back(digit);
         }
     }
 
     explicit UInt(const std::string &number, const unsigned int &base = 10) : number(), base(base) {
         if (base > BigNumber::MAX_BASE || base < 2) {
-            throw BigNumber::Error("Invalid base.");
+            throw BigNumber::invalid_base();
         }
 
-        // Removes opening zeros
         std::string prefixed_number = number;
-        while (prefixed_number[0] == '0' && prefixed_number.length() > 1) {
-            prefixed_number = prefixed_number.substr(1);
-        }
 
         for (auto it = prefixed_number.rbegin(); it != prefixed_number.rend(); ++it) {
             const unsigned int index = ToIndex(*it);
 
             if (index >= base) {
-                throw BigNumber::Error("Invalid number.");
+                throw BigNumber::invalid_base();
             }
 
             this->number.push_back(BigNumber::symbols[index]);
         }
+
+        this->RemoveOpeningZeros();
     };
 
     UInt(const unsigned long long &number) : UInt(std::to_string(number)) {};
 
-    friend bool operator==(const UInt &a, const UInt &b) {
+    bool IsFinite() const {
+        return !this->number.empty();
+    }
+
+    friend bool operator==(const BigNumber::UInt &a, const BigNumber::UInt &b) {
         if (a.base != b.base) {
-            throw BigNumber::Error("Two numbers differs by base.");
+            throw BigNumber::base_difference();
         }
 
         if (a.number.size() != b.number.size()) {
@@ -115,9 +186,9 @@ public:
         return true;
     }
 
-    friend bool operator<(const UInt &a, const UInt &b) {
+    friend bool operator<(const BigNumber::UInt &a, const BigNumber::UInt &b) {
         if (a.base != b.base) {
-            throw BigNumber::Error("Two numbers differs by base.");
+            throw BigNumber::base_difference();
         }
 
         if (a.number.empty() && b.number.empty()) {
@@ -145,7 +216,7 @@ public:
 
     }
 
-    UInt &operator=(const UInt &a) {
+    UInt &operator=(const BigNumber::UInt &a) {
         this->number.clear();
 
         for (auto &digit: a.number) {
@@ -169,49 +240,112 @@ public:
     }
 
     // Operator +
-    void operator+=(const UInt &a) {
-        if (a.base != this->base) {
-            throw BigNumber::Error("Two numbers differs by base.");
+    void operator+=(const UInt &number) {
+        if (number.base != this->base) {
+            throw BigNumber::base_difference();
         }
 
         if (this->number.empty()) {
             return;
         }
 
-        if (a.number.empty()) {
+        if (number.number.empty()) {
             this->number.clear();
             return;
         }
 
-        unsigned int carry = 0;
+        this->BasicOP(number, ADD);
+    }
 
-        auto a_it = a.number.begin();
-        auto a_end = a.number.end();
-        auto b_it = this->number.begin();
-        auto b_end = this->number.end();
+    // Operator --x
+    UInt &operator--() {
+        (*this) -= 1;
+        return *this;
+    }
 
-        while (b_it != b_end || carry) {
-            unsigned int a_index = (a_it == a.number.end() ? 0 : BigNumber::ToIndex(*a_it));
-            unsigned int b_index = (b_it == this->number.end() ? 0 : BigNumber::ToIndex(*b_it));
+    // Operator x--
+    UInt operator--(int) {
+        UInt old = UInt(*this);
+        operator--();
+        return old;
+    }
 
-            unsigned int sum = a_index + b_index + carry;
-            BigNumber::digit digit = BigNumber::symbols[(sum) % (base)];
-            carry = (sum) / base;
+    void operator-=(const UInt &number) {
+        if (this->base != number.base) {
+            throw BigNumber::base_difference();
+        }
 
-            if (a_it != a_end) {
-                ++a_it;
-            }
+        if (number.number.empty()) {
+            throw uncertainty();
+        }
 
-            if (b_it != this->number.end()) {
-                *b_it = digit;
-                ++b_it;
-            } else {
-                this->number.push_back(digit);
-            }
+        if (*this < number) {
+            this->number.clear();
+            return;
+        }
+
+        this->BasicOP(number, SUB);
+    }
+
+    void operator<<(const UInt &number) {
+        if (!number.IsFinite()) {
+            this->number.clear();
+            return;
+        }
+
+        while (number > 0) {
+            this->number.insert(this->number.begin(), BigNumber::symbols[0]);
         }
     }
 
-    void operator-=(const UInt &a) {
+    void operator>>(const UInt &number) {
+        if (!number.IsFinite()) {
+            this->number.clear();
+            this->number.push_back(BigNumber::symbols[0]);
+            return;
+        }
+
+        while (number > 0) {
+            this->number.insert(this->number.begin(), BigNumber::symbols[0]);
+        }
+    }
+
+    void operator*=(const UInt &number) {
+        if (this->base != number.base) {
+            throw BigNumber::base_difference();
+        }
+
+        if (this->number.empty()) {
+            return;
+        }
+
+        if (number.number.empty()) {
+            this->number.clear();
+            return;
+        }
+
+        BigNumber::digit digit;
+        unsigned int carry = 0;
+        unsigned int mul = 0;
+        int count = 0;
+
+        UInt copy = *this;
+
+        this->number.clear();
+        this->number.push_back(BigNumber::symbols[0]);
+
+        for (auto x = number.number.begin(); x != number.number.end(); ++x) {
+            int dec = count;
+
+            for (auto y = copy.number.begin(); y != copy.number.end(); ++y) {
+                mul = (*y) * (*x + carry);
+                digit = BigNumber::symbols[(mul) % (this->base)];
+                carry = (mul) / this->base;
+                (*this) += 0;
+            }
+            dec++;
+
+        }
 
     }
 
@@ -220,6 +354,7 @@ public:
             os << BigNumber::inf;
             return os;
         }
+
         for (auto it = n.number.rbegin(); it != n.number.rend(); ++it) {
             os << *it;
         }
@@ -228,26 +363,32 @@ public:
     }
 };
 
-BigNumber::UInt operator+(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+BigNumber::UInt BigNumber::operator+(const BigNumber::UInt &a, const BigNumber::UInt &b) {
     BigNumber::UInt result = BigNumber::UInt(a);
     result += b;
     return result;
 }
 
-bool operator>=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+BigNumber::UInt BigNumber::operator-(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+    BigNumber::UInt result = BigNumber::UInt(a);
+    result -= b;
+    return result;
+}
+
+bool BigNumber::operator>=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
     return !(a < b);
 }
 
-bool operator!=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+bool BigNumber::operator!=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
     return !(a == b);
 }
 
-bool operator<=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+bool BigNumber::operator<=(const BigNumber::UInt &a, const BigNumber::UInt &b) {
     return (a == b || a < b);
 }
 
-bool operator>(const BigNumber::UInt &a, const BigNumber::UInt &b) {
-    return !(a <= b);
+bool BigNumber::operator>(const BigNumber::UInt &a, const BigNumber::UInt &b) {
+    return !(a >= b);
 }
 
 #endif //BIG_NUMBERS_BIG_NUMBER_HPP
--- main.cpp
+++ main.cpp
@@ -3,19 +3,36 @@
 #include "big-number.hpp"
 #include<assert.h>
 
-void SumTest() {
-    std::cout << "[TEST] Operator + Testing" << std::endl;
-    BigNumber::UInt num1, num2, num3;
-    for (long long i = 0; i < 2999; ++i) {
-        assert((num1++) == i);
-        assert((++num2) == i + 1);
-        assert((num1 + num2) == (2 * (i + 1)));
+bool SumTest() {
+    BigNumber::UInt num1, num2;
+    for (long long i = 0; i < 50000; ++i) {
+        try {
+            assert((num1++) == i);
+            assert((++num2) == i + 1);
+            assert((num1 + num2) == (2 * (i + 1)));
+        } catch (...) {
+            return false;
+        }
     }
+    return true;
+}
+
+bool SubTest() {
+    BigNumber::UInt num2 = 50000;
+    unsigned int t = 50000;
+    for (long long i = 50000; i <= 0; --i) {
+        try {
+            assert((num2 - i) == (t - i));
+        } catch (...) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 int main() {
-    SumTest();
-    std::cout << "[TEST] SUM TEST: SUCCESS" << std::endl;
+    std::cout << "[TEST] OPERATOR (+):\t" << (SumTest() ? "SUCCESS" : "FAILED") << std::endl;
+    std::cout << "[TEST] OPERATOR (-):\t" << (SubTest() ? "SUCCESS" : "FAILED") << std::endl;
     return 0;
 }
